f75c32821f3627f4cf58b0f1f91a4159f03c92f5
Rathanont Sukhapant <rathanont.s@gmail.com>
2019-02-13 11:56:46 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,19 @@
+$(".popover-forget-pass").each(function (i, obj) {
+
+    $(this).popover({
+        html: true,
+        placement: 'bottom',
+        trigger: 'focus',
+        content: function () {
+            var id = $(this).attr('id')
+            return $('#popover-content-' + id).html();
+        }
+    });
+
+});
+
 $(document).ready(function () {
     $("header").sticky({
         zIndex: 1020
     });
-
 });
