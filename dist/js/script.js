6046803f134a3a0ffdcc885ff88e094547470f84
Rathanont Sukhapant <rathanont.s@gmail.com>
2019-02-22 18:55:16 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,9 +8,9 @@ $(window).scroll(function () {
 });
 
 $(function () {
-    // $("header").sticky({
-    //     zIndex: 1020,
-    // });
+    // Icons
+    $(".icon-close").html('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.034,11.034,0,22.069,11.034,11.034,0,0,11.034,11.034,22.069,0,11.034,11.034,22.069,22.069Z" transform="translate(0.965 0.965)" fill="none" stroke="#5c55e2" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.931"/></svg>');
+
     $('#artistsHotSlide').slick({
         dots: true,
         // autoplay: true,
